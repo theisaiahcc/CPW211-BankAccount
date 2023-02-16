472e8d1d70b2b3fee080888bf5f4187245605968
Isaiah C <95327043+theisaiahcc@users.noreply.github.com>
2023-02-16 08:56:19 -0800

    Add project files.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Represents a single customers bank account
+    /// </summary>
+    /// <param name="accOwner">Account owners full name</param>
+    public class Account
+    {
+        public Account(string accOwner)
+        {
+            Owner = accOwner;
+            Balance = 0;
+        }
+        /// <summary>
+        /// Account owners full name
+        /// </summary>
+        public string Owner { get; set; }
+        /// <summary>
+        /// Amount of money in account
+        /// </summary>
+        public double Balance { get; private set; }
+        /// <summary>
+        /// Adds given amount to account
+        /// </summary>
+        /// <param name="amount">amount to deposit</param>
+        public void Deposit(double amount) 
+        {
+            Balance += amount;
+        }
+        /// <summary>
+        /// Removes given amount from account
+        /// </summary>
+        /// <param name="amount">amount to take from balance</param>
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
